5720d16f7cac1dd579a22f6c21281cc1e368902b
kirbihar <kirbihar@publicisgroupe.net>
2020-05-18 21:48:39 +0530

    Updates

---
+++ precache-manifest.d651f48f060bb166b7d34a027b95b04d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ddc846e16314fa820b1fe012249b2e3",
+    "url": "/rick-morty-show/index.html"
+  },
+  {
+    "revision": "321e341b6ce43b843da6",
+    "url": "/rick-morty-show/static/css/2.e9735229.chunk.css"
+  },
+  {
+    "revision": "f2f568d288f01411f2be",
+    "url": "/rick-morty-show/static/css/main.ca43f983.chunk.css"
+  },
+  {
+    "revision": "321e341b6ce43b843da6",
+    "url": "/rick-morty-show/static/js/2.df40774d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rick-morty-show/static/js/2.df40774d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2f568d288f01411f2be",
+    "url": "/rick-morty-show/static/js/main.4a37f9e1.chunk.js"
+  },
+  {
+    "revision": "529fe6fc0b3c7405f2b2",
+    "url": "/rick-morty-show/static/js/runtime-main.3416835e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/rick-morty-show/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/rick-morty-show/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/rick-morty-show/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/rick-morty-show/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/rick-morty-show/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "d772ce88163234cfc7b82ebdd91cb642",
+    "url": "/rick-morty-show/static/media/rick-and-morty-logo.d772ce88.svg"
+  }
+]);
